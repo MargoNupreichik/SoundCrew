3fda0cdaa664f5a423a98676a40ec118f644d7d9
arlenochka <m.nupreichik@yandex.ru>
2024-11-16 15:18:50 +0300

    Finish admin interface

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ def workspace():
 
 @app.route('/m_<int:index>', methods=['get'])
 def ask_info(index):
-    phrase, param_name = get_phrase_and_param(index)
+    phrase, param_name = get_phrase_and_param_m(index)
     return render_template(f'/params_forms/manager/{param_name}.html', phrase=phrase)
 
 
 @app.route('/m_<int:index>', methods=['post'])
 def ask_info2(index):
     
-    phrase, param_name = get_phrase_and_param(index)
+    phrase, param_name = get_phrase_and_param_m(index)
     
     param = ''
     request_dict = request.form
@@ -51,16 +51,53 @@ def ask_info2(index):
     elif 'song' in request_dict:
         param = request_dict['song']
     
-    return render_template("output.html", param=param, phrase=phrase, param_type=param_name)
+    return render_template("transaction_output.html", param=param, phrase=phrase, param_type=param_name)
 
+
+@app.route('/a_<int:index>', methods=['get'])
+def insert_info(index):
+    phrase, param_name = get_phrase_and_param_a(index)
+    return render_template(f'/insert_forms/admin/{param_name}.html', phrase=phrase)
+
+
+@app.route('/a_<int:index>', methods=['post'])
+def insert_info2(index):
+    
+    phrase, param_name = get_phrase_and_param_a(index)
+    
+    param = ''
+    request_dict = request.form
+    if 'group' in request_dict:
+        param = request_dict['group']
+    elif 'song' in request_dict:
+        param = request_dict['song']
+    
+    return render_template("transaction_output.html", param=param, phrase=phrase, param_type=param_name)
+
+@app.route('/p_<int:index>')
+def print_info(index):
+    phrase = get_phrase_p(index)
+    return render_template("transaction_result.html", phrase=phrase, param_res=1)
 
 # Вспомогательные функции (часть менеджера)
-def get_phrase_and_param(index):
+def get_phrase_and_param_m(index):
     phrase = ph.managers[index]
     param = 'group'
     if index == 2:
         param = 'song'
     return phrase, param
+
+def get_phrase_and_param_a(index):
+    phrase = ph.admins[index]
+    param = 'insert_group'
+    if index == 1:
+        param = 'hit_parade'
+    if index == 2:
+        param = 'delete_group'
+    return phrase, param
+
+def get_phrase_p(index):
+    return ph.prints[index]
 
 if __name__ == '__main__':
     app.run(debug=True)
