e310285e1e6d46f0cb39d48a550f53c74053dbe9
arlenochka <m.nupreichik@yandex.ru>
2024-11-19 17:52:49 +0300

    Insert group form change

--- main.py
+++ main.py
@@ -65,14 +65,23 @@ def insert_info2(index):
     
     phrase, param_name = get_phrase_and_param_a(index)
     
-    param = ''
-    request_dict = request.form
-    if 'group' in request_dict:
-        param = request_dict['group']
-    elif 'song' in request_dict:
-        param = request_dict['song']
+    # Чтение данных о группе с формы
+    group_name = request.form.get("group")
+    founded_in = request.form.get("f_date")
+    group_country = request.form.get("country")
     
-    return render_template("transaction_output.html", param=param, phrase=phrase, param_type=param_name)
+    # Чтение значений из таблицы с музыкантами
+    fio_list = request.form.getlist('fio[]')
+    age_list = request.form.getlist('age[]')
+    position_list = request.form.getlist('position[]')
+
+    # Чтение значений из таблицы с песнями
+    title_list = request.form.getlist('title[]')
+    create_date_list = request.form.getlist('create_date[]')
+    composer_list = request.form.getlist('composer[]')
+    textwriter_list = request.form.getlist('textwriter[]')
+    
+    return render_template("transaction_output.html", param=group_name, phrase=phrase, param_type=param_name)
 
 @app.route('/p_<int:index>')
 def print_info(index):
