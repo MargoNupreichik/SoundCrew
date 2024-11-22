5b415abaa9ba98aa3e71d104b6e8878dee441567
arlenochka <m.nupreichik@yandex.ru>
2024-11-22 23:32:48 +0300

    Deliver query output to user interface

--- database_work.py
+++ database_work.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-
+from datetime import date
 engine = create_engine("mysql+mysqlconnector://root:password@localhost:3306/soundcrew", echo=True)
     
 class Manager():
@@ -9,44 +9,42 @@ class Manager():
     def get_group_info(group_name):
         
         with engine.connect() as connection:
-            query_text = "SELECT :group_name AS group_name, name, country, founded_in , hit_parade_position FROM bands WHERE name = :group;"
+            query_text = "SELECT name, country, founded_in , hit_parade_position FROM bands WHERE name = :group;"
             params = {"group": group_name}
             result = connection.execute(text(query_text), params).fetchone()
         
-        if result:
-            result = list(result)
-        else:
-            result = None
+        if result == None:
+            return None
         
-        return result
+        return list(result)
     
     # # Репертуар наиболее популярной группы
     @staticmethod
-    def most_popular_band_repertoire(band):
+    def most_popular_band_repertoire():
         
         with engine.connect() as connection:
             
             band_id_query = "SELECT id_band \
                         FROM bands \
-                        WHERE hit_parade_position = (SELECT MIN(hit_parade_position) FROM bands) LIMIT 1;"
+                        WHERE hit_parade_position = (SELECT MIN(hit_parade_position) FROM bands WHERE hit_parade_position IS NOT NULL) LIMIT 1;"
             band_id = connection.execute(text(band_id_query)).fetchone()[0]
             
             band_name_query = "SELECT name FROM bands WHERE id_band=:id"
             params = {"id": band_id}
             band = connection.execute(text(band_name_query), params).fetchone()[0]
             
-            query = "SELECT title FROM soundcrew.songs WHERE m_id_band = :band_id"
-            params = {'band_id': band_id}              
+            query = "SELECT title FROM songs WHERE m_id_band = :id"
             
             result = connection.execute(text(query), params).fetchall()
-            if result:
-                result = list(result)
-            else:
-                return None        
+            
+            if result == None:
+                return None
+            
+            fixed_result = [band]
+            for song in result:
+                fixed_result.append(list(song)[0])       
         
-        for row in result:
-            result.append(row[0])
-        return result.insert(0, band)
+        return fixed_result
     
     # Посмотреть сведения о песне (автор текса, композитор, дата создания)
     @staticmethod
@@ -62,16 +60,17 @@ class Manager():
             params = {'song_name': song}              
             
             result = connection.execute(text(query), params).fetchone()
-            if result:
-                result = list(result)
-            else:
+            
+            if result == None:
                 return None
+            
+            result = list(result)
             
             band_name_query = "SELECT name FROM bands WHERE id_band = :id"
             band_id = result.pop(-1) 
             params = {'id': band_id} 
         
-            result.insert(0,connection.execute(text(band_name_query), params).fetchone()[0])
+            result.insert(0, connection.execute(text(band_name_query), params).fetchone()[0])
         
         return result
         
@@ -81,18 +80,16 @@ class Manager():
         
         with engine.connect() as connection:
             
-            query = "SELECT :group_name as group_name, name, DATEDIFF(end_date, begin_date) AS duration_in_days, place \
+            query = "SELECT :group_name as group_name, name, begin_date, DATEDIFF(end_date, begin_date) AS duration_in_days, end_date, place \
                     FROM soundcrew.tours \
                     WHERE t_id_band = (SELECT id_band FROM soundcrew.bands WHERE name = :group_name);"
             params = {'group_name': band}              
             
             result = connection.execute(text(query), params).fetchone()
-            if result:
-                result = list(result)
-            else:
-                result = None
+            if result == None:
+                return None
         
-        return result
+        return list(result)
     
     # 'Сведения о группе (цена билета на концерт)'
     @staticmethod
@@ -134,4 +131,146 @@ class Manager():
                 return None
         
         return description
+
+class HelpInformation:
     
+    # 'Справка (лучшие группы в хит параде)'
+    @staticmethod
+    def best_hit_parade_groups():
+        
+        with engine.connect() as connection:
+            query_text = "SELECT name, hit_parade_position \
+                          FROM bands WHERE hit_parade_position IS NOT NULL\
+                          ORDER BY hit_parade_position Limit 10;"
+            result = connection.execute(text(query_text)).fetchall()
+        
+        if result:
+            group_list = list()
+            for row in result:
+                group_list.append(list(row))
+            return group_list
+        else:
+            return None
+    
+    # 'Отчет о гастролях групп'
+    @staticmethod
+    def tour_report():
+        
+        with engine.connect() as connection:
+            query_text = "SELECT bands.name AS band_name, tours.name AS tour_name, tours.begin_date, tours.end_date, tours.ticket_price \
+                        FROM tours JOIN bands ON bands.id_band = tours.t_id_band \
+                        ORDER BY tours.end_date DESC;"
+            result = connection.execute(text(query_text)).fetchall()
+        
+        if result:
+            for row in result:
+                group_list = list()
+                group_list.append(group_list)
+                for row in result:
+                    group_list.append(list(row))
+        else:
+            return None
+        
+        return group_list   
+
+class Admin:
+    
+    # 'Добавить новую группу'
+    @staticmethod
+    def insert_new_group(group_name, foundation_date, country, members_list, songs_list):
+        
+        with engine.connect() as connection:
+            transaction = connection.begin()
+            
+            try:
+                query_text = "INSERT INTO bands (name, country, founded_in) \
+                            VALUES (:group_name, :country, :founded_in);"
+                params = {"group_name": group_name, "country": country, "founded_in": foundation_date}
+                connection.execute(text(query_text), params)
+            
+                band_id_query = "SELECT id_band \
+                        FROM bands \
+                        WHERE name = :group_name;"
+                params = {"group_name": group_name}
+                band_id = connection.execute(text(band_id_query), params).fetchone()[0]
+            
+                query_text = "INSERT INTO musicians (fio, age, role, m_id_band) \
+                        VALUES (:fio, :age, :role, :m_band_id);"
+            
+                for member in members_list:
+                    params = {"fio": member[0], "age": member[1], "role": member[2], "m_band_id": band_id}
+                    connection.execute(text(query_text), params)
+            
+                query_text = "INSERT INTO songs (title, create_date, composer, textwriter, m_id_band) \
+                        VALUES (:title, :create_date, :composer, :textwriter, :m_band_id);"
+            
+                for song in songs_list:
+                
+                    inner_query_text = "SELECT id_musician \
+                        FROM musicians \
+                        WHERE fio = :musician;"
+                    params = {"musician": song[2]}
+                    composer_id = connection.execute(text(inner_query_text), params).fetchone()[0]
+                
+                    params = {"musician": song[3]}
+                    textwriter_id = connection.execute(text(inner_query_text), params).fetchone()[0]
+                
+                    params = {"title": song[0], "create_date": song[1], "composer": composer_id, "textwriter": textwriter_id, "m_band_id": band_id}
+                    connection.execute(text(query_text), params)
+            
+                transaction.commit()
+                return True
+            
+            except Exception as E:
+                transaction.rollback()
+                print(E)
+                return False
+    
+    # 'Изменить положение группы в хит-параде'
+    @staticmethod
+    def change_group_position_hit_parade(group_name, new_position):
+        
+        with engine.connect() as connection:
+            transaction = connection.begin()
+            
+            try:
+                query_text = "SELECT hit_parade_position FROM bands WHERE name = :group_name;"
+                params = {"group_name": group_name}
+                old_position = connection.execute(text(query_text), params).fetchone()            
+
+                query_text = "SELECT id_band FROM bands WHERE hit_parade_position = :new_position;"
+                params = {"new_position": new_position}
+                id_changed_position = connection.execute(text(query_text), params).fetchone()
+                   
+                query_text = "UPDATE bands SET hit_parade_position = :new_position WHERE name = :group_name;"
+                params = {"group_name": group_name, "new_position": new_position}
+                connection.execute(text(query_text), params) 
+
+                if id_changed_position != None:
+                    query_text = "UPDATE bands SET hit_parade_position = :old_position WHERE id_band = :id_changed_position;"
+                    params = {"old_position": old_position[0], "id_changed_position": id_changed_position[0]}
+                    connection.execute(text(query_text), params) 
+            
+                transaction.commit()
+                return True
+            
+            except:
+                transaction.rollback()
+                return False
+    
+    # 'Удалить информацию об исполнителе'
+    def delete_group(group_name):
+        
+        with engine.connect() as connection:
+            transaction = connection.begin()
+            
+            try:
+                query_text = "DELETE FROM bands WHERE name = :group_name;"
+                params = {"group_name": group_name}
+                connection.execute(text(query_text), params) 
+                transaction.commit()
+                return True
+            
+            except:
+                transaction.rollback()
+                return False
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ app = Flask(__name__, template_folder='templates')
 def auth():
     return render_template('/params_forms/auth/auth.html')
 
-
 @app.route('/login', methods=['post'])
 def workspace():
     login = request.form.get('log')
@@ -33,12 +32,10 @@ def workspace():
     except ValueError:
         return render_template('/params_forms/auth/auth_error.html',error=True)
 
-
 @app.route('/m_<int:index>', methods=['get'])
 def ask_info(index):
     phrase, param_name = get_phrase_and_param_m(index)
     return render_template(f'/params_forms/manager/{param_name}.html', phrase=phrase)
-
 
 @app.route('/m_<int:index>', methods=['post'])
 def ask_info2(index):
@@ -52,42 +49,73 @@ def ask_info2(index):
     elif 'song' in request_dict:
         param = request_dict['song']
     
-    return render_template("transaction_output.html", param=param, phrase=phrase, param_type=param_name)
-
+    if index == 0:
+        result = dw.Manager.get_group_info(group_name=param)
+    elif index == 1:
+        result = dw.Manager.most_popular_band_repertoire()
+    elif index == 2:
+        result = dw.Manager.get_song_info(song=param)
+    elif index == 3:
+        result = dw.Manager.get_tour_info(band=param)
+    elif index == 4:
+        result = dw.Manager.get_ticket_price(band=param)
+    elif index == 5:
+        result = dw.Manager.get_members_info(band=param)
+    
+    return render_template("transaction_output.html", param=param, phrase=phrase, param_type=param_name, result=result)
 
 @app.route('/a_<int:index>', methods=['get'])
 def insert_info(index):
     phrase, param_name = get_phrase_and_param_a(index)
     return render_template(f'/insert_forms/admin/{param_name}.html', phrase=phrase)
 
-
 @app.route('/a_<int:index>', methods=['post'])
 def insert_info2(index):
     
     phrase, param_name = get_phrase_and_param_a(index)
     
-    # Чтение данных о группе с формы
-    group_name = request.form.get("group")
-    founded_in = request.form.get("f_date")
-    group_country = request.form.get("country")
+    # Добавление новой группы
+    if index == 0:
+        # Чтение данных о группе с формы
+        group_name = request.form.get("group")
+        founded_in = request.form.get("f_date")
+        group_country = request.form.get("country")
     
-    # Чтение значений из таблицы с музыкантами
-    fio_list = request.form.getlist('fio[]')
-    age_list = request.form.getlist('age[]')
-    position_list = request.form.getlist('position[]')
+        # Чтение значений из таблицы с музыкантами
+        fio_list = request.form.getlist('fio[]')
+        age_list = request.form.getlist('age[]')
+        position_list = request.form.getlist('position[]')
+        members_list = [(fio_list[i], age_list[i], position_list[i]) for i in range(0, len(fio_list))]
 
-    # Чтение значений из таблицы с песнями
-    title_list = request.form.getlist('title[]')
-    create_date_list = request.form.getlist('create_date[]')
-    composer_list = request.form.getlist('composer[]')
-    textwriter_list = request.form.getlist('textwriter[]')
+        # Чтение значений из таблицы с песнями
+        title_list = request.form.getlist('title[]')
+        create_date_list = request.form.getlist('create_date[]')
+        composer_list = request.form.getlist('composer[]')
+        textwriter_list = request.form.getlist('textwriter[]')
+        songs_list = [(title_list[i], create_date_list[i], composer_list[i], textwriter_list[i]) for i in range(0, len(title_list))]
     
-    return render_template("transaction_output.html", param=group_name, phrase=phrase, param_type=param_name)
+        result = dw.Admin.insert_new_group(group_name=group_name, foundation_date=founded_in, country=group_country,
+                                         members_list=members_list, songs_list=songs_list)
+    # Изменение положения группы в хит-параде
+    elif index == 1:
+        group_name = request.form.get("group")
+        new_position = request.form.get("hit_i")
+        result = dw.Admin.change_group_position_hit_parade(group_name=group_name, new_position=new_position)
+    # Удаление всей информации о группе
+    elif index == 2:
+        group_name = request.form.get("group")
+        result = dw.Admin.delete_group(group_name=group_name)
+    
+    return render_template("transaction_result.html", param=result, phrase=phrase)
 
 @app.route('/p_<int:index>')
 def print_info(index):
     phrase = get_phrase_p(index)
-    return render_template("transaction_result.html", phrase=phrase, param_res=1)
+    if index == 0:
+        result = dw.HelpInformation.best_hit_parade_groups()
+    elif index == 1:
+        result = dw.HelpInformation.tour_report()
+    return render_template("transaction_output.html", phrase=phrase, result=result)
 
 # Вспомогательные функции (часть менеджера)
 def get_phrase_and_param_m(index):
@@ -95,6 +123,8 @@ def get_phrase_and_param_m(index):
     param = 'group'
     if index == 2:
         param = 'song'
+    if index == 1:
+        param = 'skip'
     return phrase, param
 
 def get_phrase_and_param_a(index):
